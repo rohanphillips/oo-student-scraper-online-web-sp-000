d7cbab7124451c7dc7ea2b741065f3f1e62a1124
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 16:17:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,40 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    return_array = []
+    student_card = doc.css(".student-card")
+    student_card.each do |card|
+      return_hash = {}
+      return_hash[:location] = card.css(".student-location").text
+      return_hash[:name] = card.css(".student-name").text
+      return_hash[:profile_url] = card.css("a").attribute("href").value
+      return_array << return_hash
+    end
+    return_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    return_hash = {}
+    a = doc.css(".social-icon-container a")
+    a.each do |hash|
+      link = hash.attribute("href").value
+      if link.include? "twitter"
+        return_hash[:twitter] = link
+      elsif link.include? "linkedin"
+        return_hash[:linkedin] = link
+      elsif link.include? "github"
+        return_hash[:github] = link
+      elsif link != ""
+        return_hash[:blog] = link
+      end
+    end
+    return_hash[:profile_quote] = doc.css(".profile-quote").text
+    return_hash[:bio] = doc.css(".description-holder p").text
+    return_hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
-class Student
+require_relative '../lib/concerns/memorable'
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+class Student
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each{|key, value| self.send(("#{key}="), value)}
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      newstudent = Student.new(student)
+      newstudent.save
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
-end
 
+end

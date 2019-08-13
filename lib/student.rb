206f6c9c55aa49c3043c15ef900ced1067608cd9
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 16:02:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each{|key, value| self.send(("#{key}="), value)}
-    all << self
+    self.all << self
   end
 
   def self.create_from_collection(students_array)

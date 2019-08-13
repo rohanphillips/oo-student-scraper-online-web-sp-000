5f61ea6f4b5963cc3d8f3c8ecd3c5c1f57810d78
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 13:27:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    css = doc.css("div class="student-card"")
+    css = doc.css(".student-card")
     binding.pry
   end
 

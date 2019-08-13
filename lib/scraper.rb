38c840b6b57be60145148fbc45190364bdf57a6c
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 15:20:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     return_hash = {}
-    a = doc.css(".social-icon-container a[value]")
+    a = doc.css(".social-icon-container a")
     binding.pry
   end
 

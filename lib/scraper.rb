aaadda0aa766cb8bfb10cc0e5ce804f369ca2fbd
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 13:22:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
